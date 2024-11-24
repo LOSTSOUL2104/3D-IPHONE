335ee8e80ce747f294b8cc5399701e516313f387
LOSTSOUL2104 <godofwar210405@gmail.com>
2024-11-24 11:15:29 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Model from "./components/Model";
 import Footer from "./components/Footer";
 import Features from "./components/Features";
 import HowItWorks from "./components/HowItWorks";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 const App = () => {
   return (
     <main className="bg-black will-change-scroll">
@@ -12,9 +13,10 @@ const App = () => {
       <Hero />
       <Highlights />
       <Model />
-      <Features/>
-      <HowItWorks/>
-      <Footer/>
+      <Features />
+      <HowItWorks />
+      <Footer />
+      <SpeedInsights />
     </main>
   );
 };
